752b4f35014deb7b63f15181568be2d778d89e66
Everardo Sánchez Hernández <everardo.ipn@gmail.com>
2018-12-23 21:32:51 -0600

    feat: improve test of parse_file

--- test/test_parse_request_file.py
+++ test/test_parse_request_file.py
@@ -11,3 +11,34 @@ class TestParseRequestFile(unittest.TestCase):
     def test_request_structure(self):
         self.assertIsInstance(requests, list)
         self.assertEqual(len(requests), 4)
+
+    def __test_request(self, req_to_test, data={}):
+        self.assertIsInstance(req_to_test, dict)
+        for key, value in data.items():
+            self.assertEqual(req_to_test[key], value)
+
+    def test_first_get_request(self):
+        self.__test_request(requests[0], {
+            "type": "get",
+            "url": "http://127.0.0.1:8000"
+        })
+
+    def test_second_post_request(self):
+        self.__test_request(requests[1], {
+            "type": "post",
+            "url": "http://127.0.0.1:8000",
+            "body": {}
+        })
+
+    def test_third_post_request(self):
+        self.__test_request(requests[2], {
+            "type": "post",
+            "url": "http://127.0.0.1:8000",
+            "body": {"user": "ever", "password": "12345"}
+        })
+
+    def test_fourth_get_request(self):
+        self.__test_request(requests[3], {
+            "type": "get",
+            "url": "http://127.0.0.1:3000"
+        })
