0d8da1fb06584afed08f5a9817d27fd3f50a9ec2
everitosan <everardo.ipn@gmail.com>
2019-01-18 18:08:29 -0600

    fix: warns when http verb is not correct

--- Request.py
+++ Request.py
@@ -2,7 +2,7 @@
 import requests
 
 # Modules
-from Logger import log_request, log_request_response, log_error
+from Logger import log_request, log_request_response, log_error, log_warning
 
 def make_request(req, verbose=False):
     res = None
@@ -11,16 +11,18 @@ def make_request(req, verbose=False):
     headers = req.get("headers")
     data = req.get("data")
 
-    req_fn = getattr(requests, type)
+    req_fn = getattr(requests, type, None)
+    if req_fn is None:
+        log_warning("[!] Method {} is not valid".format(type))
+    else:
+        log_request(req, verbose)
 
-    log_request(req, verbose)
-
-    try:
-        if type in ["get", "delete"]:
-            res = req_fn(url, headers=headers)
-        else:
-            res = req_fn(url, data=data, headers=headers)
-        log_request_response(res, verbose)
-    except requests.exceptions.ConnectionError as error:
-        log_error("[X] Connection Error")
-        log_error(error)
+        try:
+            if type in ["get", "delete"]:
+                res = req_fn(url, headers=headers)
+            else:
+                res = req_fn(url, data=data, headers=headers)
+            log_request_response(res, verbose)
+        except requests.exceptions.ConnectionError as error:
+            log_error("[X] Connection Error")
+            log_error(error)
